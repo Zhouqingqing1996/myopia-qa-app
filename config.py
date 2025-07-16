72169602d609361e04e8749e0be5bfc2d19861f4
Zhouqingqing1996 <117288278+Zhouqingqing1996@users.noreply.github.com>
2025-07-16 08:41:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ VECTOR_DB_PATH = f"faiss_index_{UNIQUE_ID}"
 DASHSCOPE_API_KEY = os.getenv("DASHSCOPE_API_KEY") 
 
 # 用于生成答案的LLM模型名称
-LLM_MODEL_NAME = "qwen-turbo-latest"
+LLM_MODEL_NAME = "deepseek-v3"
 # 用于文本嵌入的模型名称
 EMBEDDING_MODEL_NAME = "text-embedding-v2"
 
